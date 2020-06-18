91f9e5ac7f20f376257dbe7710c2aad2a19390a2
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2020-06-18 12:36:33 +0100

    DC-2577: updating dependencies and adding scalafmt (#15)

--- app/uk/gov/hmrc/emailproxy/controllers/EmailControllers.scala
+++ app/uk/gov/hmrc/emailproxy/controllers/EmailControllers.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 HM Revenue & Customs
+ * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import java.net.ConnectException
 import java.util.concurrent.TimeoutException
 
 import akka.util.ByteString
-import javax.inject.{Inject, Singleton}
+import javax.inject.{ Inject, Singleton }
 import play.api.http.HttpEntity
 import play.api.libs.json.JsValue
 import play.api.mvc._
@@ -29,34 +29,36 @@ import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import uk.gov.hmrc.play.bootstrap.controller.BackendController
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 @Singleton()
-class EmailControllers @Inject()(http: HttpClient, cc: ControllerComponents, servicesConfig: ServicesConfig)
-                                (implicit ec: ExecutionContext) extends BackendController(cc) {
+class EmailControllers @Inject()(http: HttpClient, cc: ControllerComponents, servicesConfig: ServicesConfig)(
+  implicit ec: ExecutionContext)
+    extends BackendController(cc) {
 
   private lazy val rendererBaseUrl = servicesConfig.baseUrl("email")
 
-
-  def gatewayTimeoutResult( e:Exception):Result = {
+  def gatewayTimeoutResult(e: Exception): Result = {
     val msg = s"""{ "statusCode": $BAD_GATEWAY, "message": "${e.getMessage}" } """
     Result(ResponseHeader(BAD_GATEWAY), HttpEntity.Strict(ByteString(msg), None))
       .withHeaders("Content-Type" -> "application/json")
   }
 
   def send(domain: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
-
-    http.POST(s"$rendererBaseUrl/$domain/email", request.body)
+    http
+      .POST(s"$rendererBaseUrl/$domain/email", request.body)
       .map { r =>
-        Result(ResponseHeader(r.status), HttpEntity.Strict(ByteString(r.body), r.header("contentType"))).withHeaders("Content-Type" -> "application/json")
+        Result(ResponseHeader(r.status), HttpEntity.Strict(ByteString(r.body), r.header("contentType")))
+          .withHeaders("Content-Type" -> "application/json")
       }
       .recover {
-        case e: TimeoutException => gatewayTimeoutResult(e)
-        case e: ConnectException => gatewayTimeoutResult(e)
+        case e: TimeoutException    => gatewayTimeoutResult(e)
+        case e: ConnectException    => gatewayTimeoutResult(e)
         case e: BadGatewayException => gatewayTimeoutResult(e)
 
         case e =>
-          Result(ResponseHeader(BAD_REQUEST), HttpEntity.Strict(ByteString(e.getMessage), None)).withHeaders("Content-Type" -> "application/json")
+          Result(ResponseHeader(BAD_REQUEST), HttpEntity.Strict(ByteString(e.getMessage), None))
+            .withHeaders("Content-Type" -> "application/json")
       }
   }
 
