212c224c20aeaaa4e3797192c3e5450172511e41
Kapil Sanghvi <22366606+kapil-sanghvi@users.noreply.github.com>
2021-07-20 12:28:57 +0100

    DC 3443 Upgrade bootstrap play to 4.0.0 version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,14 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "2.2.0"
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "4.0.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-26" % scope,
     "org.scalatest" %% "scalatest" % "3.0.4" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % scope,
     "org.mockito" % "mockito-core" % "3.1.0"
   )
 
